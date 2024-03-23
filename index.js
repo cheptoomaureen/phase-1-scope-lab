0d477a03e5e1be37f2e0b0ee974a066ed007b4ec
Maureen Cheptoo <cheptoomaureen31@gmail.com>
2024-03-23 18:15:05 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+console.log(customerName);
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    return customerName;
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'Jane Doe';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Alan Walker'
+}
